2916e1d7412207cb110ccbf52addac2f8cc19493
Frank-Proulx <frankproulx7@gmail.com>
2021-11-01 14:26:42 -0700

    add ui logic wip: remove is not working
    Co-authored-by: Albert Lee <leesga8@gmail.com>
    Co-authored-by: Hannah Young <hannah@corgibyte.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 function Task(description) {
   this.description = description;
   this.complete = false;
@@ -35,4 +37,30 @@ ToDoList.prototype.getTask = function(id) {
     return this.tasks[id];
   }
   return false;
+};
+
+//UI Logic
+let toDoList = new ToDoList();
+
+function updateDisplay(toDoList) {
+  $("#results").empty();
+  //replace all elements
+  for (let i = 1; i < toDoList.currentId + 1; i++) {
+    const cardTitle = "<div class='card'><div class='card-body'><h5 class='card-title'>" + toDoList.getTask(i).description + "</h5><button class='submit remove'>Remove</button></div></div>"
+    $("button.remove").click(function() {
+      toDoList.removeTask(i); 
+      updateDisplay();     
+    });
+    $("#results").append(cardTitle);
+  }
 }
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    const newTask = new Task($("#task1").val());
+    toDoList.addTask(newTask);
+    updateDisplay(toDoList);
+  });
+
+});
